4e2afff461d5625feb692f8a16de1389efc50f70
James Arnold <jamestarnold85@gmail.com>
2019-05-02 12:23:35 -0500

    added sql query

--- app/resources/list_resource.rb
+++ app/resources/list_resource.rb
@@ -1,3 +1,13 @@
 class ListResource < JSONAPI::Resource
   attributes :name
+  filters :query
+
+  def self.apply_filter(records, filter, value, options)
+    case filter
+      when :query
+        records.where('name LIKE ?', "%#{value.first}%")
+      else
+        super
+    end
+  end
 end
